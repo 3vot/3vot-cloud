b69246967d644f601c1f725732db9d385ce3bd50
Roberto Rodriguez <roberto@Robertos-MacBook-Air-2.local>
2014-05-11 15:39:20 -0600

    update create

--- app/create.js
+++ app/create.js
@@ -16,54 +16,27 @@ var promptOptions = {
   static: null
 }
 
-var tempVars = {
-  app: null,
-  options: promptOptions
-}
+
 
 function execute(options){
   var deferred = Q.defer();
   promptOptions= options;
-  tempVars.options = options;
 
   Log.debug("Creating App", "actions/app_create", 33);
 
-  createApp()
-  .then( function(){ return scaffoldSwitch() } )
-  .then( function(){ 
-    if(promptOptions.static) return true;
-    return AppInstall(promptOptions)  
-  })
-  .then( deferred.resolve )
-  .fail( deferred.reject );
-  
-  return deferred.promise;
-}
+  scaffoldSwitch()
 
-function createApp(){
-  var deferred = Q.defer();
-  
-  callbacks={
-    done: function(){
-      tempVars.app = this;
-      Log.debug("App Created Succesfully", "actions/app_create", 50);
-      return deferred.resolve(this);
-    },
-    fail: function(error){   
-      Log.debug("Error creating App Db Object", "actions/app_create", 53);
-      return deferred.reject( error )
+  process.nextTick(function(){
+    if(promptOptions.static){
+      return deferred.resolve();
     }
-  }
+    else{
+      AppInstall(promptOptions)
+      .then( deferred.resolve )
+      .fail( deferred.reject );
+    }
+  })
 
-  r = new RegExp(/^[a-z0-9-_]+$/)
-  if( r.test( promptOptions.app_name ) == false ) {
-    process.nextTick( function(){
-      return deferred.reject( "App Name " + promptOptions.app_name + " is not valid, please use only lowecase Letters, Numbers and Hypens( - )" );
-    })
-  }
-
-  App.create( { billing: { size: promptOptions.size }, name: promptOptions.app_name, public_dev_key: promptOptions.public_dev_key, user_name: promptOptions.user_name, marketing: { name: promptOptions.app_name } }, callbacks )
-  
   return deferred.promise;
 }
 
@@ -77,54 +50,50 @@ function scaffoldSwitch(){
 function scaffold(){
   Log.debug("Creating Folders and Files", "actions/app_create", 76);
 
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "app" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "assets" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "app", "assets" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "code" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "start" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "templates" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "app" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "assets" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "app", "assets" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "code" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "start" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "templates" ));
 
-  renderAndSave(Path.join( "app", "code.eco" ) , Path.join( "code" , "index.js" ), tempVars )
+  renderAndSave(Path.join( "app", "code.eco" ) , Path.join( "code" , "index.js" ) )
   
-  renderAndSave(Path.join( "app", "desktop.eco" ) , Path.join( "start" , "desktop.js" ), tempVars )
-  renderAndSave(Path.join( "app", "tablet.eco" ) , Path.join( "start" , "tablet.js" ), tempVars )
-  renderAndSave(Path.join( "app", "phone.eco" ) , Path.join( "start" , "phone.js" ), tempVars )
+  renderAndSave(Path.join( "app", "desktop.eco" ) , Path.join( "start" , "desktop.js" ) )
+  renderAndSave(Path.join( "app", "tablet.eco" ) , Path.join( "start" , "tablet.js" ) )
+  renderAndSave(Path.join( "app", "phone.eco" ) , Path.join( "start" , "phone.js" ) )
   
-  renderAndSave(Path.join( "app", "layout.eco" ) , Path.join( "templates" , "layout.html" ), tempVars )
-  renderAndSave(Path.join( "app", "head.eco" ) , Path.join( "templates" , "head.html" ), tempVars )
+  renderAndSave(Path.join( "app", "layout.eco" ) , Path.join( "templates" , "layout.html" ) )
+  renderAndSave(Path.join( "app", "head.eco" ) , Path.join( "templates" , "head.html" ) )
 
-  renderAndSave(Path.join( "app", "3vot.eco" ) , Path.join( "start", "3vot.js" ), tempVars )
+  renderAndSave(Path.join( "app", "3vot.eco" ) , Path.join( "start", "3vot.js" ) )
 
-  renderAndSave(Path.join( "app", "package.eco" ) , Path.join( "package.json" ), tempVars )
+  renderAndSave(Path.join( "app", "package.eco" ) , Path.join( "package.json" ) )
 
-
-  return tempVars.app;
+  return true;
 }
 
 function scaffoldStatic(){
   Log.debug("Scaffolding Static App", "actions/app_create", 104);
 
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "app" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "assets" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "app", "assets" ));
-  fs.mkdirSync( Path.join( process.cwd(), "apps", tempVars.app.name , "static" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "app" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "assets" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "app", "assets" ));
+  fs.mkdirSync( Path.join( process.cwd(), "apps", promptOptions.app_name , "static" ));
 
-  renderAndSave(Path.join( "app", "package.eco" ) , Path.join( "package.json" ), tempVars )
+  renderAndSave(Path.join( "app", "package.eco" ) , Path.join( "package.json" ) )
 
-  return tempVars.app;
+  return true;
 }
 
-function renderAndSave(templatePath, destPath, tempVars){
+function renderAndSave(templatePath, destPath){
   var templatesPath =  Path.join(Path.dirname(fs.realpathSync(__filename)), '../templates');
   templatePath = fs.readFileSync(  Path.join( templatesPath, templatePath ), "utf-8");
-  
-	
 
-  var templateRender = eco.render( templatePath , tempVars);
-  fs.writeFileSync( Path.join( process.cwd(), "apps", tempVars.app.name, destPath ), templateRender );
-  
+  var templateRender = eco.render( templatePath, promptOptions );
+  fs.writeFileSync( Path.join( process.cwd(), "apps", promptOptions.app_name, destPath ), templateRender );
 }
 
 module.exports = execute;
