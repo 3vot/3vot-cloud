be50b254cace801e7e12e6aa114b3a548c57562f
Roberto Rodriguez <roberto@rodriguezartavia.com>
2014-06-25 18:55:41 -0600

    update remove note

--- app/publish.js
+++ app/publish.js
@@ -29,7 +29,6 @@ var tempVars= {
 
 function execute(options){
   Log.info("Publishing Apps to the 3VOT Platform")
-  Log.info("NOTE: DO NOT REFRESH THE BROWER WHILE PUBLISHING")
 
   var deferred = Q.defer();
 
